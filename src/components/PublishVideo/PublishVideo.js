15f23e36be03538297bc418e4998c63771a9c640
Etherealnique <dominique.cortesiano@gmail.com>
2021-11-02 19:35:32 -0400

    Completed styling for Upload page

--- src/components/PublishVideo/PublishVideo.js
+++ src/components/PublishVideo/PublishVideo.js
@@ -1,11 +1,42 @@
 // import React, { Component } from "react";
 import React from "react";
 import "./PublishVideo.scss";
+import thumbnail from "../../assets/images/venice-canal.jpg";
 
 export default function PublishVideo() {
   return (
-    <div>
-      <h1>Upload Video</h1>
-    </div>
+    <main className="publish-video">
+      <div className="publish-video__container">
+        <h1 className="publish-video__title-main">Upload Video</h1>
+        <p className="publish-video__title-thumbnail">VIDEO THUMBNAIL</p>
+        <img
+          className="publish-video__thumbnail"
+          alt="venice-canal"
+          src={thumbnail}
+        />
+        <section className="publish-video__subsection">
+          <p className="publish-video__subtitle-video">TITLE YOUR VIDEO</p>
+          <input
+            className="publish-video__subtitle-video--comment"
+            type="text"
+            placeholder="Add a title to your video"
+          ></input>
+          <p className="publish-video__subtitle-description">
+            ADD A VIDEO DESCRIPTION
+          </p>
+          <input
+            className="publish-video__subtitle-description--comment"
+            type="text"
+            placeholder="Add a description to your video"
+          ></input>
+        </section>
+        <button className="publish-video__button--publish" type="submit">
+          PUBLISH
+        </button>
+        <button className="publish-video__button--cancel" type="submit">
+          CANCEL
+        </button>
+      </div>
+    </main>
   );
 }
